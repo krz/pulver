9fc9a9e477e67812f8fd5f2bf999203b650b6d52
Sophie Molnos <somolnos@gmail.com>
2016-07-22 17:03:39 +0200

    Add test cases for row and column names.

--- R/pulver.R
+++ R/pulver.R
@@ -1,3 +1,8 @@
 create_example_input_data <- function(row_names, column_names) {
-    as.data.frame(matrix(ncol = length(column_names), nrow = length(row_names)))
+    data <- as.data.frame(matrix(
+        ncol = length(column_names),
+        nrow = length(row_names)))
+    colnames(data) <- column_names
+    rownames(data) <- row_names
+    data
 }
--- tests/testthat/test-create_example_input_data.R
+++ tests/testthat/test-create_example_input_data.R
@@ -5,11 +5,11 @@ column_names <- c("id1", "id2", "id3")
 data <- create_example_input_data(row_names = row_names, column_names = column_names)
 
 test_that("columns", {
-    expect_equal(ncol(data), length(column_names))
+    expect_equal(colnames(data), column_names)
 })
 
 test_that("rows", {
-    expect_equal(nrow(data), length(row_names))
+    expect_equal(rownames(data), row_names)
 })
 
 test_that("we get a data.frame", {
