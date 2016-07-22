525d0b20078a6ab371795219ec375a6d7f094c9e
Sophie Molnos <somolnos@gmail.com>
2016-07-22 17:48:19 +0200

    Use double colon to access rnorm from stats.

--- R/pulver.R
+++ R/pulver.R
@@ -1,5 +1,5 @@
 create_example_input_data <- function(row_names, column_names) {
-    data <- matrix(rnorm(length(column_names) * length(row_names)),
+    data <- matrix(stats::rnorm(length(column_names) * length(row_names)),
         ncol = length(column_names),
         nrow = length(row_names))
     colnames(data) <- column_names
